0f02b19086f805f144fdc8329c10273fb54c12ee
Adam Mallul <RaveMaker@users.noreply.github.com>
2013-09-11 21:18:13 +0200

    Create Disable-Inactive-AD-Users.ps1

---
+++ Disable-Inactive-AD-Users.ps1
@@ -0,0 +1,31 @@
+# Disable all Active Directory users in a domain or a specified OU that have not logged on within X days and save the list to a csv file
+#
+# by RaveMaker - http://ravemaker.net
+
+# Log file
+$LogFolder = "c:\scripts\"
+$LogFile = $LogFolder + "DisableInactiveADUsers"
+
+# Search base - Domain or Organizational Unit to search - use Distinguished Name (DN)
+$OU = "OU=MyUsers,DC=domain,DC=com"
+
+# Number of Days a user has not logged on.   
+$UnusedDays = 365
+
+# Get current date in a sortable format
+$CurrentDate = Get-Date -format yyyy_MM_dd
+
+# Get the Current Date minus UnusedDays
+$DateLastActive = (Get-Date) - (new-timespan -days $UnusedDays)
+
+# Convert Current Date to Time Stamp
+$DateLastActive = (Get-Date $DateLastActive).ToFileTime()
+
+# Add current date to log file name
+$LogFile = $LogFile + "-" + $CurrentDate + ".csv"
+
+# Get Users and output to log file
+Get-ADUser -Filter { lastLogonTimestamp -lt $DateLastActive } -SearchBase "$OU" -Prop DistinguishedName,lastLogonTimestamp | Select DistinguishedName,@{n="lastLogonDate";e={[datetime]::FromFileTime($_.lastLogonTimestamp)}} | Out-File $LogFile
+
+# Disable users after creating the list
+Get-ADUser -Filter { lastLogonTimestamp -lt $DateLastActive } -SearchBase "$OU" | Disable-ADAccount
